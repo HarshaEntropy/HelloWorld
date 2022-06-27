94abdf7255934e6643dd6d7bb16fc887ae653af9
HarshaEntropy <harshavardhan.g@imaginnovate.com>
2022-06-27 18:31:38 +0530

    Micro Stricher

---
+++ mike.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double n=4;
+        double m=7;
+        System.out.println("Returned String: " + Math.ulp(m));
+        System.out.println("Return value:"+Math.sqrt(n));
+        System.out.println("Multiplying numbers :"+n*m);
+    }
+}
+
